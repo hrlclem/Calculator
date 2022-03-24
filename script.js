22c30da79a0b0ac0f0274a2ef25fa8587e4ddc18
Clement <perso@clementhurel.fr>
2022-03-24 23:19:34 +0900

    calculator

--- script.js
+++ script.js
@@ -10,23 +10,30 @@ let currentOperation = "";
 
 
 // PSEUDOCODE
+
+// OR just set currentInputA then if number is clicked, currentInputA += clicked button value.
+
+// Operation function:
 // if -savedInputA == NULL (it means it's the first input value )
-//    If a number is clicked, push related value to -currentInputA array:
+//    If a numberBtn is clicked, push related value to -currentInputA array:
 //    Click 2, push 2 to Array, click 3, push 3 to Array, click 7, push 7 to Array
 //    => currentInputA = [2, 3, 7]
 //
-//    When character is clicked (- + * /), change array to integer value '237' as -savedInputA, change -currentInputA = [] 
+//    When opeBtn is clicked (- + * /), change array to type integer value '237' as -savedInputA, change -currentInputA = [] 
 //    and sets -characterInput to "+", "-", ...
 //
 //    If = is clicked, nothing happens
 //
+//
+//
 //  if -savedInputA > 0 (it means it's the second input value )
-//    If a number is clicked, push related value to -currentInputB array:
+//    If a numberBtn is clicked, push related value to -currentInputB array:
 //    Click 4, push 4 to Array, click 6, push 6 to Array, click 1, push 1 to Array
 //    => currentInputB = [4, 6, 1]
 //
-//    When character is clicked (= ou - + * /), change array to integer value '461' as -savedInputB, change -currentInputB = [] 
-//    and sets -characterInput to "+", "-", ...
+//    When opeBtn ou equal is clicked (= ou - + * /), change array to type integer value '461' as -savedInputB, change -currentInputB = [] 
+//    and sets -characterInput to "+", "-", ... ou '' is = is clicked.
+//    Run the related operation function (add, substract...) depending on case, and return VALUE
 //
 //
 //    Make the whole operation as a string and set to currentOperation (to be able to save the whole operation string)
