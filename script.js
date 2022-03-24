ff1fa74c44b2c544e5a7f7c222b849f859dd8daa
Clement <perso@clementhurel.fr>
2022-03-24 19:47:28 +0900

    calculator

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+// Calculation functions
+const add = function(a, b) {
+	result = a + b;
+  return result;
+};
+
+const subtract = function(a, b) {
+	result = a - b;
+  return result;
+};
+
+const multiply = function(a, b) {
+	result = a * b;
+  return result;
+};
+
+const divide = function(a, b) {
+	result = a / b;
+  return result;
+};
+
+
+
+// const multiplyArray = function(array) {
+//     let result = 1;
+//       for (let i = 0; i < array.length; i++) {
+//       result *= array[i];
+//     }
+//     return result;
+//   };
+
+
+
+  
+
+// const sum = function(array) {
+//   let result = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     result += array[i] 
+//   }
+//   return result;
+// };
+
